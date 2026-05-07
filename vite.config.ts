b00c3d1eda76537e0c1934c17305a3d1cdcc03fd
Karina Klages <karinaklages@web.de>
2026-05-07 13:59:10 +0200

    feat: paths fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/memory-game/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ``
+      }
+    }
+  }
 })
